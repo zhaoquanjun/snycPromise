d50d621c3ff7f995f9ca2d29e00109a9b7cc94f5
zhaoquanjun <15910559620@163.com>
2023-03-04 12:52:32 +0800

    update

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // 消除异步传染性
 // 重点是通过抛错来进行多次请求达到同步效果
 const syncPromise = (func) => {
+  if (typeof func !== 'function') {
+    throw Error('please confirm the param to be a function')
+  }
   let cache = []
   let i = 0
-  const _fetch = window.fetch
+  window._fetch = window._fetch || window.fetch
   window.fetch = (...args) => {
     if (cache[i]) {
       if (cache[i].status == 'fulfilled') {
@@ -12,6 +15,7 @@ const syncPromise = (func) => {
       if (cache[i].status == 'rejected') {
         throw cache[i].err
       }
+      window.fetch = window._fetch
     } else {
       // 发送请求 然后报错
       const r = {
